dd581399c4195741479a420f46e379be7795d0d4
armoredchimp <mattkasmir@gmail.com>
2023-09-05 20:39:02 -0700

    2nd

--- learning.py
+++ learning.py
@@ -1,1 +1,21 @@
 print("Hello World!")
+
+print("Hello!\nHello new Line!\nAnd the third.")
+
+print("Hello" + " Matt")
+print("Hello"+""+"Matt")
+
+print("Hello" + input("Your name?!"))
+# comment
+
+name = input("what's your name?")
+print(name)
+name = "Matt"  # replaces previous name
+length = len(name)
+print(length)
+
+print("Welcome to the Band Name Generator.\nWhat's the name of the city you grew up in?\n")
+city = input()
+print("What's your pet's name?\n")
+pet = input()
+print("Your band name could be " + city + pet)
