65015fcafc428f6ee16cbcc0849ca87f4e0b8224
armoredchimp <mattkasmir@gmail.com>
2023-09-15 14:43:01 -0700

    9/15b

--- learning.py
+++ learning.py
@@ -113,16 +113,39 @@
 
 
 # Leap Year
-year = int(input("Enter year:"))
-leap = False
-if year % 4 == 0:
-    leap = True
-    if year % 100 == 0:
-        leap = False
-        if year % 400 == 0:
-            leap = True
+# year = int(input("Enter year:"))
+# leap = False
+# if year % 4 == 0:
+#     leap = True
+#     if year % 100 == 0:
+#         leap = False
+#         if year % 400 == 0:
+#             leap = True
 
-if leap == True:
-    print("Leap year.")
-else:
-    print("Not leap year.")
+# if leap == True:
+#     print("Leap year.")
+# else:
+#     print("Not leap year.")
+
+
+# Pizza Order
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+bill = 0
+if size == "S":
+    bill += 15
+elif size == "M":
+    bill += 20
+elif size == "L":
+    bill += 25
+if add_pepperoni == "Y":
+    if (size == "M" or size == "L"):
+        bill += 3
+    else:
+        bill += 2
+if extra_cheese == "Y":
+    bill += 1
+
+print(f"Your final bill is: ${bill}.")
