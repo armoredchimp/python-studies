021aa6135dcdd9440ecdda94f556eb4ad37067b0
armoredchimp <mattkasmir@gmail.com>
2023-09-20 15:33:53 -0700

    9/20

--- learning.py
+++ learning.py
@@ -400,9 +400,54 @@ import py_module
 #         highest = score
 # print(f"The highest score in the class is: {highest}")
 
-for number in range(1, 11, 3):
-    print(number)
+# for number in range(1, 11, 3):
+#     print(number)
 
-total = 0
-for number in range(1, 101):
-    total += number
+# total = 0
+# for number in range(1, 101):
+#     total += number
+
+# final = 0
+# for number in range(0, 101, 2):
+#     final += number
+#     print(number)
+# print(final)
+
+# FizzBuzz
+# for number in range(1, 100):
+#     if ((number % 3 == 0) and (number % 5 == 0)):
+#         print("FizzBuzz")
+#     elif (number % 5 == 0):
+#         print("Buzz")
+#     elif (number % 3 == 0):
+#         print("Fizz")
+#     else:
+#         print(number)
+
+# Password generator - did this before I knew there was a shuffle(), that would have been helpfuL!
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+           'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+password = []
+# final = []
+for number in range(0, nr_letters):
+    password.append(random.choice(letters))
+for number in range(0, nr_symbols):
+    password.append(random.choice(symbols))
+for number in range(0, nr_numbers):
+    password.append(random.choice(numbers))
+# for number in range(0, len(password)):
+#     char = random.choice(password)
+#     password.remove(char)
+#     final.append(char)
+
+random.shuffle(password)
+password = ''.join(password)
+print(password)
