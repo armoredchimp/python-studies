6a60cd87738f9978aea1a335946a8c66228da666
armoredchimp <mattkasmir@gmail.com>
2023-09-21 20:07:25 -0700

    9/21

--- learning.py
+++ learning.py
@@ -426,28 +426,143 @@ import py_module
 
 # Password generator - did this before I knew there was a shuffle(), that would have been helpfuL!
 import random
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
-           'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+# letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+#            'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+# numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+# symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-print("Welcome to the PyPassword Generator!")
-nr_letters = int(input("How many letters would you like in your password?\n"))
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
-password = []
-# final = []
-for number in range(0, nr_letters):
-    password.append(random.choice(letters))
-for number in range(0, nr_symbols):
-    password.append(random.choice(symbols))
-for number in range(0, nr_numbers):
-    password.append(random.choice(numbers))
-# for number in range(0, len(password)):
-#     char = random.choice(password)
-#     password.remove(char)
-#     final.append(char)
+# print("Welcome to the PyPassword Generator!")
+# nr_letters = int(input("How many letters would you like in your password?\n"))
+# nr_symbols = int(input(f"How many symbols would you like?\n"))
+# nr_numbers = int(input(f"How many numbers would you like?\n"))
+# password = []
+# # final = []
+# for number in range(0, nr_letters):
+#     password.append(random.choice(letters))
+# for number in range(0, nr_symbols):
+#     password.append(random.choice(symbols))
+# for number in range(0, nr_numbers):
+#     password.append(random.choice(numbers))
+# # for number in range(0, len(password)):
+# #     char = random.choice(password)
+# #     password.remove(char)
+# #     final.append(char)
 
-random.shuffle(password)
-password = ''.join(password)
-print(password)
+# random.shuffle(password)
+# password = ''.join(password)
+# print(password)
+
+# # Day 6
+
+
+# def my_function():
+#     print("Hello")
+
+
+# Hangman
+stages = ['''
+ 
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+ 
+''', '''
+ 
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+ 
+''', '''
+ 
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+ 
+''', '''
+ 
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+ 
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+ 
+''', '''
+ 
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+ 
+''', '''
+ 
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+ 
+''']
+word_list = ["penguin", "monkey", "castle", "palace"]
+random_word = random.choice(word_list)
+revealed_word = []
+guesses = 6
+
+for letter in random_word:
+    revealed_word.append("_")
+
+for char in revealed_word:
+    print(f"{char} ", end="")
+
+while guesses > 0:
+    count = 0
+    guess = input("\nGuess a letter:").lower()
+    correct_guess = False
+    for letter in random_word:
+        if guess == letter:
+            print(f"{letter} ", end="")
+            count += 1
+            revealed_word[count - 1] = guess
+            correct_guess = True
+        else:
+            count += 1
+            print(f"{revealed_word[count - 1]} ", end="")
+    if correct_guess == False:
+        guesses -= 1
+        print(stages[guesses])
+    if guesses == 0:
+        if list(random_word) == revealed_word:
+            print("\nVictory!")
+            exit()
+        else:
+            print("\nDefeat!")
+            exit()
+    if list(random_word) == revealed_word:
+        print("\nVictory!")
+        exit()
