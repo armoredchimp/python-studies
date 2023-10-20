6d3a8a2e9b76f7062101635c177e3cde4d7e4c5d
armoredchimp <mattkasmir@gmail.com>
2023-10-20 14:36:50 -0700

    10/20

--- Day20/main.py
+++ Day20/main.py
@@ -1,9 +1,14 @@
 from turtle import Turtle, Screen
+import time
+
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+screen.tracer(0)
+
+segments = []
 
 
 def createSnake(n):
@@ -11,10 +16,25 @@ def createSnake(n):
     for _ in range(n):
         turtle = Turtle('square')
         turtle.color('white')
-        turtle.speed(0)
+        # turtle.speed(0)
+        turtle.penup()
         turtle.goto(x, 0)
         x -= 20
+        segments.append(turtle)
 
 
 createSnake(10)
+
+gameActive = True
+for _ in range(0, 20):
+    screen.update()
+    time.sleep(0.1)
+
+    for seg in range(len(segments) - 1, 0, -1):
+        new_x = segments[seg - 1].xcor()
+        new_y = segments[seg - 1].ycor()
+        segments[seg].goto(new_x, new_y)
+    segments[0].forward(20)
+
+
 screen.exitonclick()
