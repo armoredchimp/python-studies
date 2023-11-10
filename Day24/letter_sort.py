00295671e781e463f9f90369622d586b9b595766
armoredchimp <mattkasmir@gmail.com>
2023-11-10 10:31:14 -0800

    11/10

--- Day24/letter_sort.py
+++ Day24/letter_sort.py
@@ -1,13 +1,10 @@
 names_array = []
 
 with open('Names/invited_names.txt', mode='r') as file:
-    names = file.read()
-    names_array = names.split('\n')
+    names_array = file.read().split('\n')
 with open('Letters/starting_letter.txt', mode='r') as file:
     letter = file.read()
 
-print(names_array)
-print(len(names_array))
 
 for name in names_array:
     with open(f'Letters/letter_for_{name}.txt', mode='w') as file:
