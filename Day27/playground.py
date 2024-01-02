fd433bcd22a7abf67bd23636d0edc19ed367575b
armoredchimp <mattkasmir@gmail.com>
2024-01-02 15:10:32 -0800

    1/2

--- Day27/playground.py
+++ Day27/playground.py
@@ -3,3 +3,25 @@ def add(*args):
     for arg in args:
         sum += arg
     return sum
+
+
+def calculate(n, **kwargs):
+    print(kwargs)
+    # for key, value in kwargs.items():
+    #     print(key, value)
+
+    # print(kwargs['add'])
+    n += kwargs['add']
+    n *= kwargs['multiply']
+
+
+calculate(2, add=3, multiply=5)
+
+
+class Car:
+    def __init__(self, **kw):
+        self.make = kw.get['make']
+        self.model = kw.get['model']
+
+
+my_car = Car(make='Toyota', model='Camry')
