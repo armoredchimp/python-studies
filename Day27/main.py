d4aa9f5c98756467f432ee0e1185faa0518cf74b
armoredchimp <mattkasmir@gmail.com>
2024-01-03 16:30:55 -0800

    1/3

--- Day27/main.py
+++ Day27/main.py
@@ -6,7 +6,26 @@ window.minsize(width=500, height=300)
 
 # Label
 labool = tkinter.Label(text='I am a label', font=('Arial', 24, 'bold'))
-labool.pack(side="left")
+labool.pack()
+
+
+labool['text'] = 'new Text'
+labool.config(text='labool')
+
+# button
+
+
+def button_clicked():
+    labool.config(text=input_text.get())
+
+
+button = tkinter.Button(text='Click Me', command=button_clicked)
+button.pack()
+
+# Entry
+input_text = tkinter.StringVar()
+input = tkinter.Entry(width=10, textvariable=input_text)
+input.pack()
 
 
 window.mainloop()
